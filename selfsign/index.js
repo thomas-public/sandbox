b058372f00888a34abbb0c6d42fbe8f28e8aa416
Thomas Wong <thomas@ThomasMBP.local>
2020-05-30 00:54:00 +0800

    initial commit

---
+++ selfsign/index.js
@@ -0,0 +1,30 @@
+const fs = require( "fs" );
+const forge = require( "node-forge" );
+const subject = [ { "value": "TW", "shortName": "C" }, { "value": "TEST", "shortName": "O" }, { "value": "TEST UNIT", "shortName": "OU" }, { "value": "TEST CLIENT", "shortName": "CN" } ];
+
+const rootcaCrt = forge.pki.certificateFromPem( fs.readFileSync( "./rootca/rootca.crt.pem", 'utf-8' ) );
+const rootcaPriKey = forge.pki.privateKeyFromPem( fs.readFileSync( "./rootca/rootca.pri.pem", 'utf-8' ) );
+
+// generate client key pair and csr
+const clientKeyPair = forge.pki.rsa.generateKeyPair( 2048 );
+const csr = forge.pki.createCertificationRequest();
+csr.publicKey = clientKeyPair.publicKey;
+csr.setSubject( subject );
+csr.sign( clientKeyPair.privateKey );
+console.log( "csr:\n" + forge.pki.certificationRequestToPem( csr ) );
+
+// signing client csr
+const cert = forge.pki.createCertificate();
+cert.serialNumber = '00';
+const time = Date.now();
+cert.validity.notBefore = new Date(time);
+cert.validity.notAfter = new Date(time + 36520 * 86400 * 1000);
+
+cert.setSubject( csr.subject.attributes );
+cert.setIssuer( rootcaCrt.subject.attributes );
+cert.publicKey = csr.publicKey;
+cert.sign( rootcaPriKey, forge.md.sha256.create() );
+console.log( "crt:\n" + forge.pki.certificateToPem( cert ) );
+
+const passed = rootcaCrt.verify(cert);
+console.log("verify passed:", passed);
